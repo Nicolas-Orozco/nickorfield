33700ba7f9d8ea0a4e726d8f2b56bcc2a326c6d8
Nicolas Orozco <nicolasorozcodavila@gmail.com>
2023-02-10 17:27:10 -0500

    Add active tab classes

--- src/components/PostSorter.jsx
+++ src/components/PostSorter.jsx
@@ -10,12 +10,18 @@ function PostSorter({ allPosts }) {
   const sortedPosts = allPosts.filter((p) => p.frontmatter.tag === currentTag);
   return (
     <>
-      <div className="tabs">
-        <a onClick={() => setcurrentTag("all")} className="tab">
+      <div className="tabs tabs-boxed">
+        <a
+          onClick={() => setcurrentTag("all")}
+          className={`${currentTag === "all" && "tab-active"} tab`}
+        >
           all
         </a>
         {allPosts.map(({ frontmatter }) => (
-          <a onClick={() => setcurrentTag(frontmatter.tag)} className="tab">
+          <a
+            onClick={() => setcurrentTag(frontmatter.tag)}
+            className={`${currentTag === frontmatter.tag && "tab-active"} tab`}
+          >
             {frontmatter.tag}
           </a>
         ))}
